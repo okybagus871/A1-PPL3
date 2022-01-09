fef7cce3ac0b4e3bb026147fb671bd6b5b77f54c
okybagus871 <oky.bagus.tif20@polban.ac.id>
2022-01-09 09:02:11 +0700

    update extend user

--- FrontEnd/src/Pages/AccountInformation.js
+++ FrontEnd/src/Pages/AccountInformation.js
@@ -1,24 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity, TextInput, SafeAreaView } from 'react-native';
 import { BackIcon } from "../assets";
 import { Gap } from '../components/atoms';
 import { getData } from "../utils";
 import { getEmail } from "../utils";
 import axios from "axios";
+// import { TextInput } from "react-native-gesture-handler";
+import { showMessage } from "react-native-flash-message";
+import { Dropdown, DropDown } from "react-native-element-dropdown";
+
+const email = 'okkybagus87@gmail.com';
 
 export default function AccountInformation({ navigation }) {
 
-    const [username, setUsername] = useState([]);
-    const [name, setName] = useState([]);
-    const [joinDate, setJoinDate] = useState([]);
-    const [phone, setPhone] = useState([]);
-    const [address, setAddress] = useState([]);
-    const [identityType, setIdentityType] = useState([]);
-    const [identityNo, setIdentityNo] = useState([]);
-    const [emergencyCall, setEmergencyCall] = useState([]);
+    const dd = [
+        { label: 'KTP', value: 'KTP' },
+        { label: 'SIM', value: 'SIM' },
+        { label: 'KK', value: 'KK' },
+    ];
+    const [open, setOpen] = useState(false);
+    const [username, setUsername] = useState('');
+    const [name, setName] = useState('');
+    const [joinDate, setJoinDate] = useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
+    const [identityType, setIdentityType] = useState(null);
+    const [identityNo, setIdentityNo] = useState('');
+    const [emergencyCall, setEmergencyCall] = useState('');
+    const [kodePos, setKodePos] = useState('');
 
     const getData = async () => {
-        const email = 'okkybagus87@gmail.com';
         try {
             const res = await axios.get("http://10.0.2.2:8080/get-user", {
                 params: {
@@ -33,18 +44,54 @@ export default function AccountInformation({ navigation }) {
             setIdentityNo(res.data.identity_no);
             setIdentityType(res.data.identity_type);
             setEmergencyCall(res.data.emergency_call);
+            setKodePos(res.data.postal_code);
         }
         catch (error) {
             alert(error.message);
         }
-    };
+    }
+
+    const dropDown = () => {
+
+    }
+
+    const onSubmit = async () => {
+        try {
+            const data = {
+                email: email,
+                username: username,
+                name: name,
+                phonenumber: phone,
+                address_ktp: address,
+                postal_code: kodePos,
+                identity_type: identityType,
+                identity_no: identityNo,
+                emergency_call: emergencyCall,
+            }
+            const res = await axios.post('http://10.0.2.2:8080/update-profile', data);
+            if ((await res).data.status == true) {
+                showMessage({
+                    message: "Informasi akun berhasil diubah",
+                    type: 'info',
+                });
+            }
+            else {
+                showMessage({
+                    message: "Gagal",
+                    type: 'warning',
+                });
+            }
+        } catch (error) {
+            alert(error.message);
+        }
+    }
 
     useEffect(() => {
         getData()
     }, [])
 
     return (
-        <ScrollView style={styles.wrapper}>
+        <ScrollView style={styles.wrapper} nestedScrollEnabled={true}>
             <View style={styles.header}>
                 <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
                     <Image source={BackIcon} style={styles.iconBack} />
@@ -57,11 +104,23 @@ export default function AccountInformation({ navigation }) {
                 <View style={styles.konten}>
                     <View style={styles.kotak1}>
                         <Text style={styles.kontenKotak}>Username</Text>
-                        <Text style={styles.dataUser}>{username}</Text>
+                        {/* <Text style={styles.dataUser}>{username}</Text> */}
+                        <TextInput
+                            autoCapitalize="none"
+                            placeholder="Enter your username"
+                            value={username}
+                            onChangeText={(value) => setUsername(value)}
+                        />
                     </View>
                     <View style={styles.kotak}>
                         <Text style={styles.kontenKotak}>Name</Text>
-                        <Text style={styles.dataUser}>{name}</Text>
+                        {/* <Text style={styles.dataUser}>{name}</Text> */}
+                        <TextInput
+                            autoCapitalize="none"
+                            placeholder="Enter your name"
+                            value={name}
+                            onChangeText={(value) => setName(value)}
+                        />
                     </View>
                 </View>
                 <View style={styles.konten}>
@@ -71,44 +130,126 @@ export default function AccountInformation({ navigation }) {
                     </View>
                     <View style={styles.kotak}>
                         <Text style={styles.kontenKotak}>Phone Number</Text>
-                        <Text style={styles.dataUser}>{phone}</Text>
+                        <TextInput
+                            keyboardType="numeric"
+                            autoCapitalize="none"
+                            placeholder="Enter your phone number"
+                            value={phone}
+                            onChangeText={(value) => setPhone(value)}
+                        />
                     </View>
                 </View>
                 <View style={styles.konten}>
                     <View style={styles.kotakDua}>
                         <Text style={styles.kontenKotak}>Alamat</Text>
-                        <Text style={styles.dataUser}>{address}</Text>
+                        {/* <Text style={styles.dataUser}>{address}</Text> */}
+                        <TextInput
+                            autoCapitalize="none"
+                            placeholder="Enter your address"
+                            value={address}
+                            onChangeText={(value) => setAddress(value)}
+                        />
                     </View>
                 </View>
                 <View style={styles.konten}>
                     <View style={styles.kotakDua}>
                         <Text style={styles.kontenKotak}>Kode Pos</Text>
+                        <TextInput
+                            autoCapitalize="none"
+                            placeholder="Enter your postal code"
+                            value={kodePos}
+                            onChangeText={(value) => setKodePos(value)}
+                        />
                     </View>
                 </View>
                 <View style={styles.konten}>
                     <View style={styles.kotakDua}>
                         <Text style={styles.kontenKotak}>Jenis Identitas</Text>
-                        <Text style={styles.dataUser}>{identityType}</Text>
+                        <Dropdown
+                            style={styles.dropdown}
+                            placeholderStyle={styles.placeholderStyle}
+                            selectedTextStyle={styles.selectedTextStyle}
+                            inputSearchStyle={styles.inputSearchStyle}
+                            iconStyle={styles.iconStyle}
+                            data={dd}
+                            maxHeight={169}
+                            labelField="label"
+                            valueField="value"
+                            placeholder="Select Item"
+                            value={identityType}
+                            onChange={item=>{
+                                setIdentityType(item.value);
+                            }}
+                        />
                     </View>
                 </View>
                 <View style={styles.konten}>
                     <View style={styles.kotakDua}>
                         <Text style={styles.kontenKotak}>Nomor Identitas</Text>
-                        <Text style={styles.dataUser}>{identityNo}</Text>
+                        <TextInput
+                            autoCapitalize="none"
+                            placeholder="Enter your identity number"
+                            value={identityNo}
+                            onChangeText={(value) => setIdentityNo(value)}
+                        />
                     </View>
                 </View>
                 <View style={styles.konten}>
                     <View style={styles.kotakAkhir}>
                         <Text style={styles.kontenKotak}>Emergency Call</Text>
-                        <Text style={styles.dataUser}>{emergencyCall}</Text>
+                        <TextInput
+                            autoCapitalize="none"
+                            placeholder="Enter your emergency number"
+                            value={emergencyCall}
+                            onChangeText={(value) => setEmergencyCall(value)}
+                        />
                     </View>
                 </View>
+                <TouchableOpacity style={styles.btn} onPress={onSubmit}>
+                    <Text style={styles.textBtn}>Save</Text>
+                </TouchableOpacity>
             </View>
         </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
+    dropdown: {
+        marginHorizontal: 10,
+        height: 40,
+    },
+    icon: {
+        marginRight: 5,
+    },
+    placeholderStyle: {
+        fontSize: 16,
+    },
+    selectedTextStyle: {
+        fontSize: 16,
+    },
+    iconStyle: {
+        width: 20,
+        height: 20,
+    },
+    inputSearchStyle: {
+        height: 40,
+        fontSize: 16,
+    },
+    textBtn: {
+        color: 'white',
+        fontSize: 17,
+        fontFamily: 'RobotoMedium',
+        textAlign: 'center',
+    },
+    btn: {
+        margin: 20,
+        borderRadius: 20,
+        borderWidth: 1,
+        backgroundColor: '#0c8eff',
+        borderColor: '#80979797',
+        justifyContent: 'center',
+        height: 60,
+    },
     dataUser: {
         padding: 5,
         color: 'black',
